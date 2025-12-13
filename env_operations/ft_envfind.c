5e1b3cb03de5ecd6125b1f6ac4831ecc43560a36
Rafik Babayan <20rafik05babayan@gmail.com>
2025-12-13 13:58:36 +0400

    pre-merge commit

--- env_operations/ft_envfind.c
+++ env_operations/ft_envfind.c
@@ -6,7 +6,7 @@
 /*   By: rababaya <rababaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 16:04:23 by rababaya          #+#    #+#             */
-/*   Updated: 2025/10/13 14:46:04 by rababaya         ###   ########.fr       */
+/*   Updated: 2025/12/05 23:05:56 by rababaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_env	*ft_envfind(t_env *env_list, char *key)
 {
-	while (env_list && ft_strncmp(env_list->key, key, ft_strlen(key)))
+	while (env_list && ft_strncmp(env_list->key, key, ft_strlen(key) + 1))
 		env_list = env_list->next;
 	return (env_list);
 }
