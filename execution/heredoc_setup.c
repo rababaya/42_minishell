9c2525ea62607d5ec731f2c7958f049a098b10fa
Rafik Babayan <20rafik05babayan@gmail.com>
2026-01-17 18:32:07 +0400

    fix heredoc norm errors

--- execution/heredoc_setup.c
+++ execution/heredoc_setup.c
@@ -38,12 +38,49 @@ int	count_heredocs(t_data *data)
 	return (len);
 }
 
-int	open_heredocs(t_data *data)
+static int	process_heredoc(t_data *data, t_tkn *tkn, int i, int init)
+{
+	int	ret;
+
+	if (init && (data->hrdc)[i].fd != 0)
+		close((data->hrdc)[i].fd);
+	ret = heredoc(data, tkn, i);
+	if (ret)
+		return (ret);
+	return (0);
+}
+
+static int	process_tokens(t_data *data, int *i, int *init)
 {
 	t_tkn	*tkn;
-	int		i;
-	int		init;
 	int		ret;
+
+	tkn = data->tkn_list;
+	while (tkn)
+	{
+		if (tkn->type == PIPE && *init)
+		{
+			data->hrdc[(*i)++].tkn = tkn;
+			*init = 0;
+		}
+		else if (tkn->type == HRDC)
+		{
+			ret = process_heredoc(data, tkn, *i, *init);
+			if (ret)
+				return (ret);
+			*init = 1;
+		}
+		tkn = tkn->next;
+	}
+	if (*init)
+		data->hrdc[*i].tkn = tkn;
+	return (0);
+}
+
+int	open_heredocs(t_data *data)
+{
+	int	i;
+	int	init;
 
 	i = count_heredocs(data);
 	if (!i)
@@ -51,28 +88,7 @@ int	open_heredocs(t_data *data)
 	data->hrdc = (t_hrdc *)malloc(sizeof(t_hrdc) * i);
 	if (!data->hrdc)
 		return (1);
-	tkn = data->tkn_list;
 	i = 0;
 	init = 0;
-	while (tkn)
-	{
-		if (tkn->type == PIPE && init)
-		{
-			data->hrdc[i++].tkn = tkn;
-			init = 0;
-		}
-		else if (tkn->type == HRDC)
-		{
-			if (init && (data->hrdc)[i].fd != 0)
-				close((data->hrdc)[i].fd);
-			ret = heredoc(data, tkn, i);
-			if (ret)
-				return (ret);
-			init = 1;
-		}
-		tkn = tkn->next;
-	}
-	if (init)
-		data->hrdc[i].tkn = tkn;
-	return (0);
+	return (process_tokens(data, &i, &init));
 }
